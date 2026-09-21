119e68ad7687fce489f6e929de3f7aa89300aeda
hugorouillard <89408328+hugorouillard@users.noreply.github.com>
2026-09-21 22:43:11 +0200

    fix(build): target new name

--- scripts/build-web.sh
+++ scripts/build-web.sh
@@ -5,4 +5,4 @@ cargo build --locked --release --target wasm32-unknown-unknown
 
 mkdir -p dist
 cp web/index.html dist/index.html
-cp target/wasm32-unknown-unknown/release/pocket-orrery.wasm dist/pocket-orrery.wasm
+cp target/wasm32-unknown-unknown/release/solanum.wasm dist/solanum.wasm
